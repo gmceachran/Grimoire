7548d59c1704405946006301de7923371fb71ebb
Gabe McEachran <gmceachran@gmail.com>
2025-06-02 15:34:20 -0500

    Add line break after chapter link in addChapter function to improve layout in app.js.

--- grimoire/app.js
+++ grimoire/app.js
@@ -2,12 +2,13 @@
   const ul = document.getElementById('contents-list');
   const chapterName = document.getElementById('chapter-name').value;
   const chapterLink = document.createElement('a');
-
+  
   chapterLink.href = 'chapter.html';
   chapterLink.id = chapterName;
   chapterLink.textContent = chapterName;
  
   ul.appendChild(chapterLink);
+  ul.appendChild(document.createElement('br'));
   document.getElementById('chapter-name').value = '';
  };
 
